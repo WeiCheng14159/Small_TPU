022aff281a15b051e8b31b8a42357e8ed4b9f231
Wei Cheng <michael1996623@gmail.com>
2023-04-19 20:13:23 +0800

    fix-Remove unused

--- sw/utils/decode_fp32.py
+++ sw/utils/decode_fp32.py
@@ -1,9 +1,5 @@
 import numpy as np
 
-# def decode_fp32(data):
-#     hex_map = ['0', '1', '2', '3', '4', '5', '6',
-#                 '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
-
 
 def float_to_fixed(array, min_val=-1.0, max_val=1.0, num_bits=8):
     # Calculate the range of values that can be represented in the fixed point format
