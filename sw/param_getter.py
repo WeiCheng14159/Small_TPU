b0b547915fd0a6535b1efd606b09df2d5ce5a193
Wei Cheng <michael1996623@gmail.com>
2023-04-27 22:41:19 +0800

    feat-Finalize fixed point quantization scheme

--- sw/param_getter.py
+++ sw/param_getter.py
@@ -9,12 +9,14 @@ from models.model_zoo import model_zoo
 # Utilities
 from visualize.visualize_dist import plot_distribution
 from utils.save_to_file import save_weights, save_inputs, save_outputs
+from utils.quantization_scheme import QuantizationScheme
 
 
 class ParamGetter:
 
-    def __init__(self, model):
+    def __init__(self, model, q_scheme):
         self.model = model
+        self.q_scheme = q_scheme
 
     def get_weights(self, layer_name):
         # Iterate through layers of interests and store weight tensors
@@ -22,7 +24,7 @@ class ParamGetter:
             if layer.name == layer_name:
                 tensor_w = layer.get_weights()[0]
                 print(f"Layer {layer_name} weights shape: {tensor_w.shape}")
-                save_weights(tensor_w, layer_name+"_w.txt")
+                save_weights(tensor_w, layer_name+"_w.txt", self.q_scheme)
                 # Plot weight distributions and save as an image
                 # plot_distribution(tensor_w, str(layer_name+"_w.png"))
 
@@ -32,7 +34,7 @@ class ParamGetter:
             if layer.name == layer_name:
                 tensor_b = layer.get_weights()[1]
                 print(f"Layer {layer_name} bias shape: {tensor_b.shape}")
-                save_weights(tensor_b, layer_name+"_bias.txt")
+                save_weights(tensor_b, layer_name+"_bias.txt", self.q_scheme)
                 # Plot weight distributions and save as an image
                 # plot_distribution(tensor_b, str(layer_name+"_bias.png"))
 
@@ -43,50 +45,55 @@ class ParamGetter:
         tensor_out = temp_model.predict(tensor_in)
         print(f"Layer {layer_name} output shape: {tensor_out.shape}")
         # Save outputs as text file
-        save_weights(tensor_out, layer_name+"_out.txt")
+        save_weights(tensor_out, layer_name+"_out.txt", self.q_scheme)
         # Plot weight distributions and save as an image
         # plot_distribution(tensor_out, str(layer_name+"_out.png"))
 
 
-if len(sys.argv) < 2:
-    print("Usage: python param_getter.py <loaded_model_name.h5>")
-    sys.exit(1)
+if __name__ == "__main__":
 
-# Get the model name from command line argument
-model_filename = str(sys.argv[1])
-model_name = model_filename.replace(".h5", "")
+    if len(sys.argv) < 2:
+        print("Usage: python param_getter.py <loaded_model_name.h5>")
+        sys.exit(1)
 
-# Define the input shape of the network
-input_shape = (28, 28, 1)
-# Define the number of classes
-num_classes = 10
+    # Get the model name from command line argument
+    model_filename = str(sys.argv[1])
+    model_name = model_filename.replace(".h5", "")
 
-if model_name not in model_zoo():
-    print(
-        f"Model '{model_name}' not found. Please choose from {list(model_zoo().keys())}.")
-    sys.exit(1)
+    # Define the input shape of the network
+    input_shape = (28, 28, 1)
+    # Define the number of classes
+    num_classes = 10
 
-model = model_zoo()[model_name](
-    input_shape=input_shape, num_classes=num_classes)
-model.load_weights(model_filename)
-model.summary()
+    if model_name not in model_zoo():
+        print(
+            f"Model '{model_name}' not found. Please choose from {list(model_zoo().keys())}.")
+        sys.exit(1)
 
-# Specify the names of the layers whose weights you want to print
-specific_layers = ['quant_fc1', 'quant_fc2', 'quant_fc3']
+    model = model_zoo()[model_name](
+        input_shape=input_shape, num_classes=num_classes)
+    model.load_weights(model_filename)
+    model.summary()
 
-# Create a random input tensor for testing
-# input_x = np.random.rand(1, 28, 28, 1)
+    # Specify the names of the layers whose weights you want to print
+    specific_layers = ['quant_fc1', 'quant_fc2', 'quant_fc3']
 
-# Use mnist dataset for testing
-(x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
-input_x = x_train[0].reshape(-1, 28, 28, 1)
+    # Create a random input tensor for testing
+    # input_x = np.random.rand(1, 28, 28, 1)
 
-# Save inputs (of format: NHWC) as text file
-save_inputs(input_x, "input.txt")
+    # Use mnist dataset for testing
+    (x_train, y_train), (x_test, y_test) = datasets.mnist.load_data()
+    input_x = x_train[0].reshape(-1, 28, 28, 1)
 
-getter = ParamGetter(model)
+    # Quantization scheme
+    q_scheme = QuantizationScheme(8, 3)
 
-for layer in specific_layers:
-    getter.get_weights(layer)
-    getter.get_bias(layer)
-    getter.get_outputs(input_x, layer)
+    # Save inputs (of format: NHWC) as text file
+    save_inputs(input_x, "input.txt", q_scheme)
+
+    getter = ParamGetter(model, q_scheme)
+
+    for layer in specific_layers:
+        getter.get_weights(layer)
+        getter.get_bias(layer)
+        getter.get_outputs(input_x, layer)
--- sw/utils/save_to_file.py
+++ sw/utils/save_to_file.py
@@ -1,27 +1,54 @@
 import numpy as np
+from .quantization_scheme import QuantizationScheme
 
 
-def save_weights(w, filename):
+def save_weights(w, filename, q_scheme):
     data = w.flatten()
-    write_float32(data, filename)
+    write_float32(data, q_scheme, filename)
 
 
-def save_inputs(inp, filename):
+def save_inputs(inp, filename, q_scheme):
     data = inp.flatten()
-    write_float32(data, filename)
+    write_float32(data, q_scheme, filename)
 
 
-def save_outputs(outp, filename):
+def save_outputs(outp, filename, q_scheme):
     data = outp.flatten()
-    write_float32(data, filename)
+    write_float32(data, q_scheme, filename)
 
 
-def write_float32(data, filename):
+def write_float32(data, q_scheme, filename):
     if not isinstance(data, np.ndarray):
         raise ("This is not an numpy ndarray")
     if len(data.shape) != 1:
         raise ("numpy ndarray is not flattened")
+    q_data = q_scheme.convert(data)
+    # Mutiply a scale to move decimal points
+    q_data = q_data * (2**q_scheme.frac_bits)
+    # Convert to integer
+    q_data = q_data.astype(int)
+
     with open(filename, 'w') as f:
-        for i in range(data.shape[0]):
-            f.write('{:.8f}\n'.format(data[i]))
+        num_of_hex = 8
+        for i in range(q_data.shape[0]):
+            data = q_data[i]
+            # f.write("float32 {:.8f}\n".format(data))
+            if data < 0:  # Negative case
+                # Only chop out the bits we quantized
+                lsb_mask = (1 << q_scheme.total_bits) - 1
+                # Pad the most significant bits with ones
+                msb_mask = ~lsb_mask
+                # Mask out only the bits we want to print
+                print_mask = (1 << (4 * num_of_hex)) - 1
+                padded_with_1s = (
+                    # In 2's compliment, (~num + 1) equals to sign conversion
+                    ~(~data + 1) + 1
+                ) & lsb_mask \
+                    | msb_mask \
+                    & print_mask
+                f.write("{data:0{width}X}\n".format(
+                    width=num_of_hex, data=padded_with_1s))
+            else:  # Positive case
+                f.write("{data:0{width}X}\n".format(
+                    width=num_of_hex, data=data))
     f.close()
