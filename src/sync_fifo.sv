50a3b44024b52a0b81197c6dcc4ba6b835f76ad5
Wei Cheng <michael1996623@gmail.com>
2023-05-02 12:39:42 +0800

    feat-Add sync FIFO as buffer for systolic array

---
+++ src/sync_fifo.sv
@@ -0,0 +1,51 @@
+
+`ifndef SYNC_FIFO_SV
+`define SYNC_FIFO_SV
+
+module SyncFifo #(
+    parameter DEPTH = 8,
+    parameter WIDTH = 16
+) (
+    input logic clk,
+    rstn,
+    input logic w_en,
+    r_en,
+    input logic [WIDTH-1:0] data_in,
+    output logic [WIDTH-1:0] data_out,
+    output logic full,
+    empty
+);
+
+  logic [$clog2(DEPTH)-1:0] w_ptr, r_ptr;
+  logic [WIDTH-1:0] fifo[DEPTH];
+
+  // Set Default values on reset.
+  always @(posedge clk) begin
+    if (!rstn) begin
+      w_ptr <= 0;
+      r_ptr <= 0;
+      data_out <= 0;
+    end
+  end
+
+  // To write data to FIFO
+  always @(posedge clk) begin
+    if (w_en & !full) begin
+      fifo[w_ptr] <= data_in;
+      w_ptr <= w_ptr + 1;
+    end
+  end
+
+  // To read data from FIFO
+  always @(posedge clk) begin
+    if (r_en & !empty) begin
+      data_out <= fifo[r_ptr];
+      r_ptr <= r_ptr + 1;
+    end
+  end
+
+  assign full  = ((w_ptr + 1'b1) == r_ptr);
+  assign empty = (w_ptr == r_ptr);
+endmodule
+
+`endif
--- src/systolic_array_controller.sv
+++ src/systolic_array_controller.sv
@@ -1,5 +1,6 @@
 `include "include/define.v"
 `include "systolic_array/systolic_array.sv"
+`include "sync_fifo.sv"
 
 module SystolicArrayController (
     input  logic                            clk,
@@ -21,15 +22,97 @@ module SystolicArrayController (
 
   // TODO: Implement necessary buffer 
 
+  // array_enb
+  localparam FSM_IDLE = 0, FSM_REQ = 1, FSM_HALT = 2, FSM_END = 3;
+  logic [0:FSM_END] sa_curr_state, sa_next_state;
+  logic sa_step, fifo_load, fifo_ready;
+
+  always_ff @(posedge clk) begin
+    if (~rstn) sa_curr_state <= 0;
+    else sa_curr_state <= sa_next_state;
+  end
+
+  always_comb begin
+    sa_next_state <= 0;
+    case (1'b1)
+      sa_curr_state[FSM_IDLE]: begin
+        sa_next_state[FSM_REQ] = (curr_state[`S_LOAD]) ? 1'b1 : 1'b0;
+      end
+      sa_curr_state[FSM_REQ]: begin
+        if (~curr_state[`S_LOAD]) sa_next_state[FSM_IDLE] = 1'b1;
+        else if (fifo_load) sa_next_state[FSM_HALT] = 1'b1;
+        else sa_next_state[FSM_REQ] = 1'b1;
+      end
+      sa_curr_state[FSM_HALT]: begin
+        if (fifo_ready) sa_next_state[FSM_REQ] = 1'b1;
+        else sa_next_state[FSM_HALT] = 1'b1;
+      end
+      sa_curr_state[FSM_END]: begin
+        sa_next_state[FSM_END] = 1'b1;
+      end
+      default: ;
+    endcase
+  end
+
+  // Row-direction fifo 
+  logic [TILE_DIM-1:0] row_fifo_wen, row_fifo_ren;
+  logic [0:`DATA_WIDTH-1] row_fifo_from_mem[0:TILE_DIM-1];
+  logic [0:`DATA_WIDTH-1] row_fifo_to_array[0:TILE_DIM-1];
+  logic [0:TILE_DIM-1] row_fifio_full, row_fifo_empty;
+
+  SyncFifo #(
+      .DEPTH(8),
+      .WIDTH(`DATA_WIDTH)
+  ) i_RowSyncFifo[0:TILE_DIM-1] (
+      .clk(clk),
+      .rstn(rstn),
+      .w_en(row_fifo_wen),
+      .r_en(row_fifo_ren),
+      .data_in(row_fifo_from_mem),
+      .data_out(row_fifo_to_array),
+      .full(row_fifio_full),
+      .empty(row_fifo_empty)
+  );
+
+  // Column-direction fifo 
+  logic [TILE_DIM-1:0] col_fifo_wen, col_fifo_ren;
+  logic [0:`DATA_WIDTH-1] col_fifo_from_mem[0:TILE_DIM-1];
+  logic [0:`DATA_WIDTH-1] col_fifo_to_array[0:TILE_DIM-1];
+  logic [0:TILE_DIM-1] col_fifio_full, col_fifo_empty;
+
+  SyncFifo #(
+      .DEPTH(8),
+      .WIDTH(`DATA_WIDTH)
+  ) i_ColumnSyncFiFo[0:TILE_DIM-1] (
+      .clk(clk),
+      .rstn(rstn),
+      .w_en(col_fifo_wen),
+      .r_en(col_fifo_ren),
+      .data_in(col_fifo_from_mem),
+      .data_out(col_fifo_to_array),
+      .full(col_fifio_full),
+      .empty(col_fifo_empty)
+  );
+
+  // Connection between fifo and systolic array 
+  genvar i;
+  generate
+    for (i = 0; i < TILE_DIM; i++) begin : CONNECT_FIFO_TO_ARRAY
+      assign row_i[i*`DATA_WIDTH+:`DATA_WIDTH] = row_fifo_to_array[i];
+      assign col_i[i*`DATA_WIDTH+:`DATA_WIDTH] = col_fifo_to_array[i];
+    end
+  endgenerate
+
   SystolicArray #(
       .TILE_DIM(TILE_DIM)
   ) i_SystolicArray (
       .clk(clk),
       .rstn(rstn),
-      .enb(array_enb),
+      .enb(sa_step),
       .in_row(row_i),
       .in_col(col_i),
       .out(out_r)
   );
+
 
 endmodule
